f89b9b00632d012efeeebbec945410ae7262e17a
Radu Dita <hades2510@yahoo.com>
2015-06-23 23:21:17 +0300

    using configs to drive the tutorial

--- start.py
+++ start.py
@@ -2,30 +2,66 @@ import sys
 import os
 import codecs
 import time
+import json
+import pprint
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 
 if getattr(sys, 'frozen', False):
-	basedir = sys._MEIPASS
+    basedir = sys._MEIPASS
 else:
-	basedir = '.'
+    basedir = '.'
 
-chrome_driver_path = os.path.join(basedir,os.path.join('libs','chromedriver'))
+def find_elem(browser, locator):
+    
+    if locator.startswith("#"):
+        elem = browser.find_element_by_id(locator[1:]) 
+    elif locator.startswith("."):            
+        elem = browser.find_element_by_class_name(locator[1:])
 
-c_o = Options()
-c_o.binary_location = os.path.join(basedir,"cefsimple.app/Contents/MacOS/cefsimple")
+    return elem
 
-browser = webdriver.Chrome(executable_path=chrome_driver_path,chrome_options=c_o)
-browser.get("http://dev01.owpdev.com:8080/owp")
+def run_tut(steps, config):
+    
+    wait_in_mili = tut_config["metadata"]["default_wait"]
+    highlight_elem = tut_config["metadata"]
 
-time.sleep(1)
+    chrome_driver_path = os.path.join(basedir,os.path.join('libs','chromedriver'))
 
-pas = browser.find_element_by_id("pw")
-pas.send_keys("secret")
+    c_o = Options()
+    c_o.binary_location = os.path.join(basedir,os.path.join('libs',"cefsimple.app/Contents/MacOS/cefsimple"))
+    
+    browser = webdriver.Chrome(executable_path=chrome_driver_path,chrome_options=c_o)
 
-uname = browser.find_element_by_id("un")
-uname.send_keys("admin")
-uname.send_keys(Keys.RETURN)
+    for step in steps:
+        if step["type"] == "url":
+            print "going to url ",step["data"]
+            browser.get(step["data"])
+            print "loaded url ",step["data"]
 
+        if step["type"] == "text_input":
+
+            data = step["data"]
+
+            elem = find_elem(browser, step["data"]["locator"])
+            
+            print "feeding input ",data            
+            
+            elem.send_keys(data["value"])
+
+        if step["type"] == "button_press":
+            
+            data = step["data"]
+            
+            elem = find_elem(browser, data["locator"])
+            
+            print "pushing button ",data
+
+            elem.click()
+
+        time.sleep(wait_in_mili/1000.0)            
+
+tut_config = json.load( open("configs/ow.tut") )
+
+run_tut(tut_config["steps"], tut_config["metadata"])
